7f23d688afcf87c2730bb14a1572ee95fb2abaf1
s1ntaxe770r <cyberdev01@protonmail.com>
2023-08-25 16:51:06 +0100

    test web example

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"os"
 
 	"github.com/buildpacks/pack/pkg/client"
 	"github.com/buildpacks/pack/pkg/image"
@@ -17,36 +16,27 @@ func main() {
 	if err != nil {
 		logrus.Error(err)
 	}
-		
+
 	envs := make(map[string]string)
-	
+
 	envs["CGO_ENABLED"] = "0"
 	envs["BP_GO_BUILD_FLAGS"] = "-buildmode=default"
-	
-	wd, err := os.Getwd()
 
+	// should be the equivalent of  pack build my-app  --buildpack paketo-buildpacks/go  --builder paketobuildpacks/builder-jammy-buildpackless-static
+	buildOpts := client.BuildOptions{
+		Image:      "web-example",
+		Builder:    "paketobuildpacks/builder-jammy-full",
+		Buildpacks: []string{"paketo-buildpacks/web-servers"},
+		PullPolicy: image.PullIfNotPresent,
+		AppPath:    "./public",
+		// DockerHost: "unix:///var/run/docker.sock",
+		Env: map[string]string{
+			"BP_WEB_SERVER": "nginx",
+		},
+	}
+	err = c.Build(ctx, buildOpts)
 	if err != nil {
 		logrus.Error(err)
-		return
 	}
-
-
-    // should be the equivalent of  pack build my-app  --buildpack paketo-buildpacks/go  --builder paketobuildpacks/builder-jammy-buildpackless-static
-    buildOpts := client.BuildOptions{
-        Image:           "pong",
-        Builder:         "paketobuildpacks/builder-jammy-buildpackless-static",
-        Buildpacks:      []string{"paketo-buildpacks/go"},
-        AppPath:         "./sample",
-        PullPolicy:      image.PullIfNotPresent,
-        RelativeBaseDir: wd + "/sample",
-        DockerHost:      "unix:///var/run/docker.sock",
-        TrustBuilder:    client.IsSuggestedBuilderFunc,
-        Env:             envs,
-        LifecycleImage:  "buildpacksio/lifecycle",
-    }
-    err = c.Build(ctx, buildOpts)
-    if err != nil {
-        logrus.Error(err)
-    }
 
 }
